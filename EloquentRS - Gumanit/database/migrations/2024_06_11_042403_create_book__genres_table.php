621687c524f4258851a857ceafe9e4444e9ab1b9
HonkaROO <christiangumanit@gmail.com>
2024-06-18 21:57:48 +0800

    Add files via upload

---
+++ EloquentRS - Gumanit/database/migrations/2024_06_11_042403_create_book__genres_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBookGenresTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('book__genres', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('book_id')->constrained()->onDelete('cascade');
+            $table->foreignId('genre_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('book__genres');
+    }
+};
